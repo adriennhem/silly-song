263571b2c27efbf7e0fcfe71ef19db601f71c2b4
Adrien Nhem <adriennhem@pushers-macbook-pro.local>
2017-11-13 11:21:24 +0000

    Initial Commit

---
+++ Silly Song/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Silly Song
+//
+//  Created by Adrien Nhem on 13/11/2017.
+//  Copyright © 2017 Adrien Nhem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
